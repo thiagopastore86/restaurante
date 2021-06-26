73233b5d74006adf57e6590c80a1a1ece75a990d
Thiago Pastore <thiagopastore86@gmail.com>
2021-06-26 10:34:06 -0300

    Projeto criado

---
+++ src/test/java/com/tpastoresolucoes/restaurante/RestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpastoresolucoes.restaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
